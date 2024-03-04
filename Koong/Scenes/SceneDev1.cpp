b6937712a62dee4aa274fecfee36b208baa2914d
DESKTOP-8GHFKI9\KGA <frostpine2214@gmail.com>
2024-03-04 18:00:57 +0900

    no message

--- Koong/Scenes/SceneDev1.cpp
+++ Koong/Scenes/SceneDev1.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "SceneDev1.h"
+#include "AniTest.h"
+#include "Ground.h"
 
 SceneDev1::SceneDev1(SceneIds id) : Scene(id)
 {
@@ -11,7 +13,13 @@ SceneDev1::~SceneDev1()
 
 void SceneDev1::Init()
 {
-	
+	AddGo(new AniTest());
+	AddGo(new Ground());
+
+	Scene::Init();
+
+
+
 }
 
 void SceneDev1::Release()
@@ -21,6 +29,14 @@ void SceneDev1::Release()
 
 void SceneDev1::Enter()
 {
+	sf::Vector2f windowSize = (sf::Vector2f)FRAMEWORK.GetWindowSize();
+	sf::Vector2f centerPos = windowSize * 0.5f;
+	worldView.setSize(windowSize);
+	worldView.setCenter({ 0.f, 0.f });
+	uiView.setSize(windowSize);
+	uiView.setCenter(centerPos);
+
+
 	Scene::Enter();
 }
 
@@ -33,8 +49,4 @@ void SceneDev1::Update(float dt)
 {
 	Scene::Update(dt);
 
-	if (InputMgr::GetKeyDown(sf::Keyboard::Space))
-	{
-		SceneMgr::Instance().ChangeScene(SceneIds::SceneDev2);
-	}
 }
